f8ab49abbe207230c28e7c4ce6e34c6f2c8fd715
Tornado <164438219+realtornado@users.noreply.github.com>
2024-03-22 17:34:53 +0300

    Delete src directory

--- src/Commands/PrefixCommands/User/komut2.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'komut2',
-    description: 'Deneme 1 Komutu 1',
-    execute(client, message, args) {
-        message.reply('Deneme 1 Komutu 1 çalıştı!');
-    },
-};
--- src/database/models/deneme.js
+++
@@ -1,7 +0,0 @@
-const {model, Schema, Types} = require("mongoose")
-
-module.exports = model("deneme", new Schema(
-    {
-        guild: String
-    }
-))
