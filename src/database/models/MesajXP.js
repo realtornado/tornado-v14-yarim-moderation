ae1dbb7072f15a8809c1f5260e13f96697e20a7c
Tornado <164438219+realtornado@users.noreply.github.com>
2024-03-29 18:32:46 +0300

    Delete src directory

--- src/database/models/MesajXP.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const { model, Schema } = mongoose;
-
-const MesajXPDB = mongoose.models["MesajXP"] || model("MesajXP", new Schema({ author: String, xp: String, messageCount: String, channels: [{ channelId: String, channelName: String, messageCount: Number }]
-}));
-
-module.exports = MesajXPDB;
